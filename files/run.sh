699da45bc82c272496ac120f25141946697c9cfd
Jeremy Nguyen Xuan <jnguyenxuan@lbl.gov>
2016-11-23 09:30:26 -0800

    corrected jar name

--- files/run.sh
+++ files/run.sh
@@ -11,7 +11,7 @@ wget -O /data/scigraph.tgz http://scigraph-data-dev.monarchinitiative.org/static
 cd /data/ && tar xzfv scigraph.tgz
 mkdir -p /data/json
 /data/solr-6.2.1/bin/solr start
-cd /data/golr-loader && java -Xmx50G -Dlog4j.configuration=file:/data/log4j.properties -jar target/golr-loader-0.0.1-SNAPSHOT-jar-with-dependencies.jar -g /data/graph.yaml -q /data/monarch-cypher-queries/src/main/cypher/feature-location -o /data/json/ -s http://localhost:8983/solr/feature-location
+cd /data/golr-loader && java -Xmx100G -Dlog4j.configuration=file:/data/log4j.properties -jar target/golr-loader-0.0.1-SNAPSHOT.jar -g /data/graph.yaml -q /data/monarch-cypher-queries/src/main/cypher/feature-location -o /data/json/ -s http://localhost:8983/solr/feature-location
 /data/solr-6.2.1/bin/solr stop
 cd /data/solr-6.2.1/server/solr && tar czfv feature-location.tgz feature-location/
 cp /data/solr-6.2.1/server/solr/feature-location.tgz /solr
