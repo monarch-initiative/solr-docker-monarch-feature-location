ccb294f9e8d452008211d632ae78087f4a9e0f75
Kent Shefchek <kshefchek@gmail.com>
2019-03-18 09:11:59 -0700

    optimize

--- files/run.sh
+++ files/run.sh
@@ -12,6 +12,7 @@ cd /data/ && tar xzfv scigraph.tgz
 mkdir -p /data/json
 /data/solr-6.2.1/bin/solr start
 cd /data/golr-loader && java -Xmx100G -Dlogback.configurationFile=file:/data/logback.xml -jar target/golr-loader-0.0.1-SNAPSHOT.jar -g /data/graph.yaml -q /data/monarch-cypher-queries/src/main/cypher/feature-location -s http://localhost:8983/solr/feature-location
+curl http://localhost:8983/solr/feature-location/update?optimize=true
 /data/solr-6.2.1/bin/solr stop
 cd /data/solr-6.2.1/server/solr && tar czfv feature-location.tar feature-location/
 cp /data/solr-6.2.1/server/solr/feature-location.tar /solr
